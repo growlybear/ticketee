87535d0fde60b1d43628f95c0d32b9e0fd8fde46
Michael Allan <michaelallan@optushome.com.au>
2012-09-13 16:43:14 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'ba019e97d5fdb7ccadc099a3c5f25aa6acd1cf2d7969d169042f3b31a39752a451a8b705967c0c4169c932751cd42ab2a09d2dad757c3d63f85016970713a8a3'
